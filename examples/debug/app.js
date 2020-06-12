313a0d453ac75c55ee4529dc9e975009d6100890
gpmao <gpmao@iflytek.com>
2020-06-12 17:58:04 +0800

    组件加载尝试

--- examples/debug/app.js
+++ examples/debug/app.js
@@ -1,3 +1,13 @@
+
+Vue.component('content-type', {
+  template: '#modal-template',
+  props: {
+    ccc: {
+      type: String,
+      default: '0000'
+    }
+  }
+})
 var app = new Vue({
   el: '#app',
   data() {
@@ -5,7 +15,8 @@ var app = new Vue({
       text: '123',
       list: [
         1,2,3
-      ]
+      ],
+      type: 'type'
     }
   }
 })
